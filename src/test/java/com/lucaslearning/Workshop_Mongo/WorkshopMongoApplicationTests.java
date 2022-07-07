35e3f15618619d00c7ac2a819d287ad2b64d993b
Lucas Côrte <lucascorte@outlook.com>
2022-07-06 22:04:55 -0300

    first commit - project created

---
+++ src/test/java/com/lucaslearning/Workshop_Mongo/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaslearning.Workshop_Mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
