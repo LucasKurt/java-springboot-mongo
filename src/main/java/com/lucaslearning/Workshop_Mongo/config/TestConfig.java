788105e943f29ac5f1030ff106eb1f71098e9ab0
Lucas Côrte <lucascorte@outlook.com>
2022-07-09 17:21:26 -0300

    Post entity  with nested user

--- src/main/java/com/lucaslearning/Workshop_Mongo/config/TestConfig.java
+++ src/main/java/com/lucaslearning/Workshop_Mongo/config/TestConfig.java
@@ -1,13 +1,17 @@
 package com.lucaslearning.Workshop_Mongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.lucaslearning.Workshop_Mongo.entities.Post;
 import com.lucaslearning.Workshop_Mongo.entities.User;
+import com.lucaslearning.Workshop_Mongo.repositories.PostRepository;
 import com.lucaslearning.Workshop_Mongo.repositories.UserRepository;
 
 @Configuration
@@ -16,9 +20,15 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository; 
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
 		
@@ -27,6 +37,15 @@ public class TestConfig implements CommandLineRunner {
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		postRepository.deleteAll();
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"),"Bom dia", "Acordei feliz hoje!", maria);
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		
 	}
 
 }
