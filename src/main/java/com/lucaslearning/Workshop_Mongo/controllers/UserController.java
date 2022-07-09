78797ed947cfe745054ccdf0ac0ac14e8b059f9e
Lucas Côrte <lucascorte@outlook.com>
2022-07-09 14:43:02 -0300

    Delete user

--- src/main/java/com/lucaslearning/Workshop_Mongo/controllers/UserController.java
+++ src/main/java/com/lucaslearning/Workshop_Mongo/controllers/UserController.java
@@ -1,14 +1,19 @@
 package com.lucaslearning.Workshop_Mongo.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucaslearning.Workshop_Mongo.dto.UserDTO;
 import com.lucaslearning.Workshop_Mongo.entities.User;
@@ -32,5 +37,19 @@ public class UserController {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User user = userService.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody UserDTO userDTO) {
+		User user = userService.fromDTO(userDTO);
+		user = userService.insertUser(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/lucaslearning/Workshop_Mongo/services/UserService.java
+++ src/main/java/com/lucaslearning/Workshop_Mongo/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lucaslearning.Workshop_Mongo.dto.UserDTO;
 import com.lucaslearning.Workshop_Mongo.entities.User;
 import com.lucaslearning.Workshop_Mongo.repositories.UserRepository;
 import com.lucaslearning.Workshop_Mongo.services.exception.ResourceNotFoundException;
@@ -23,5 +24,18 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ResourceNotFoundException("User: " + id + " not found"));
+	}
+	
+	public User insertUser(User user) {
+		return userRepository.insert(user);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail()); 
 	}
 }
